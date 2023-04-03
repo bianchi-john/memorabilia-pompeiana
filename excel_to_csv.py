ad8017572d538673f14bdf4fde2130828bc723eb
john-lenci-bianchi <john.lenci.bianchi@gmail.com>
2023-04-03 15:03:21 +0200

    All features have been developed but have not yet been tested within the site

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -45,7 +45,7 @@ def datiAnagrafica(fileIn, fileOut):
                 # Stampo il contenuto
                 # print (cell_content)
 
-                # Se c'è qualche valora nella riga allora:
+                # Se c'è qualche valore nella riga allora:
                 if (cell_content[0]):
                     # Sostituisco il contenuto Null con la stringa 'Missing Value'
                     for i in range(len(cell_content)):
@@ -101,7 +101,7 @@ def datiScavo(fileIn, fileOut):
                 # Stampo il contenuto
                 # print (cell_content)
 
-                # Se c'è qualche valora nella riga allora:
+                # Se c'è qualche valore nella riga allora:
                 if (cell_content[0]):
                     # Sostituisco il contenuto Null con la stringa 'Missing Value'
                     for i in range(len(cell_content)):
@@ -109,7 +109,7 @@ def datiScavo(fileIn, fileOut):
                             cell_content[i] = 'Missing Value'
 
                     # Scrivo il contenuto
-                    writer.writerow([cell_content[0], cell_content[1], cell_content[2], cell_content[3], cell_content[4], cell_content[5], cell_content[6], cell_content[7], cell_content[8], cell_content[9], cell_content[10],
+                    writer.writerow([cell_content[0], float(cell_content[1]), cell_content[2], cell_content[3], cell_content[4], cell_content[5], cell_content[6], cell_content[7], cell_content[8], cell_content[9], cell_content[10],
                                 cell_content[11], cell_content[12], cell_content[13], cell_content[14], cell_content[15], cell_content[16], cell_content[17], cell_content[18]])
     except: 
         print('C\'è stato un errore duranta la creazione del file: \"' + fileOut + '\"')
@@ -155,7 +155,7 @@ def datiCollezionistici(fileIn, fileOut):
                 # Stampo il contenuto
                 # print (cell_content)
 
-                # Se c'è qualche valora nella riga allora:
+                # Se c'è qualche valore nella riga allora:
                 if (cell_content[0]):
                     # Sostituisco il contenuto Null con la stringa 'Missing Value'
                     for i in range(len(cell_content)):
@@ -163,7 +163,7 @@ def datiCollezionistici(fileIn, fileOut):
                             cell_content[i] = 'Missing Value'
 
                     # Scrivo il contenuto
-                    writer.writerow([cell_content[0], cell_content[1], cell_content[2], cell_content[3], cell_content[4], cell_content[5], cell_content[6], cell_content[7], cell_content[8], cell_content[9], cell_content[10],
+                    writer.writerow([cell_content[0], float(cell_content[1]), cell_content[2], cell_content[3], cell_content[4], cell_content[5], cell_content[6], cell_content[7], cell_content[8], cell_content[9], cell_content[10],
                                 cell_content[11], cell_content[12], cell_content[13], cell_content[14], cell_content[15], cell_content[16], cell_content[17]])
     except: 
         print('C\'è stato un errore duranta la creazione del file: \"' + fileOut + '\"')
@@ -212,7 +212,7 @@ def datiBibliografici(fileIn, fileOut):
                 # Stampo il contenuto
                 # print (cell_content)
 
-                # Se c'è qualche valora nella riga allora:
+                # Se c'è qualche valore nella riga allora:
                 if (cell_content[0]):
                     # Sostituisco il contenuto Null con la stringa 'Missing Value'
                     for i in range(len(cell_content)):
@@ -227,32 +227,179 @@ def datiBibliografici(fileIn, fileOut):
 
 def abbreviazioniArchivi(fileIn, fileOut):
     try:
-        # Da fare, occhio che è diverso il file perché non ha i numeri e anche
-        # alla codifica dei caratteri che è strana, vedi "Berlino nella seconda riga"
-        print('')
+        # Definisci gli header per il tuo file CSV
+        headers = ['','Abbreviazione','Archivio','']
+
+        # Apri il file CSV in modalità scrittura
+        directory = "dataJohn"  # Specifico la directory
+        filename = fileOut  # Specifico il nome del file
+
+        if not os.path.exists(directory):  # Creo la directory se non esiste
+            os.makedirs(directory)
+
+        filePathAndName = os.path.join(
+            directory, filename)  # Creo il full name path
+
+        with open(filePathAndName, 'w', newline='') as file:
+            # Creo un oggetto writer per scrivere nel file CSV
+            writer = csv.writer(file)
+            # Apro il file Excel
+            workbook = openpyxl.load_workbook(fileIn)
+
+            # Seleziono il foglio di lavoro che vuoi leggere
+            worksheet = workbook['Abbreviazioni Archivi']
+
+            # Scrivo gli header nel file CSV
+            writer.writerow(headers)
+
+            #Inizializzo un contatore che stamperò nella prima colonna e mi serve da indice
+            contatore = 0
+
+            # Leggo i dati nell'excel ciclicamente e li salvo in un oggetto per poi metterli nel CSV
+            for row in worksheet.iter_rows(min_row=0, min_col=0, max_col=3):
+
+                # Creo una lista vuota per contenere il contenuto delle celle
+                cell_content = []
+
+                # Itero su ogni cella e aggiungi il contenuto alla lista
+                for cell in row:
+                    cell_content.append(cell.value)
+
+                # Stampo il contenuto
+                # print (cell_content)
+
+                # Se c'è qualche valore nella riga allora:
+                if (cell_content[0]):
+                    # Sostituisco il contenuto Null con la stringa 'Missing Value'
+                    for i in range(len(cell_content)):
+                        if cell_content[i] is None:
+                            cell_content[i] = 'Missing Value'
+
+                    # Scrivo il contenuto
+                    writer.writerow([contatore, cell_content[0], cell_content[1], ''])
+                    contatore += 1
     except: 
         print('C\'è stato un errore duranta la creazione del file: \"' + fileOut + '\"')
 
 
 def abbreviazioniBibliografiche(fileIn, fileOut):
     try:
-        # DA VEDERE TUTTO
-        print('')
+        # Definisci gli header per il tuo file CSV
+        headers = ['','Abbreviazione','Bibliografia','']
+
+        # Apri il file CSV in modalità scrittura
+        directory = "dataJohn"  # Specifico la directory
+        filename = fileOut  # Specifico il nome del file
+
+        if not os.path.exists(directory):  # Creo la directory se non esiste
+            os.makedirs(directory)
+
+        filePathAndName = os.path.join(
+            directory, filename)  # Creo il full name path
+
+        with open(filePathAndName, 'w', newline='') as file:
+            # Creo un oggetto writer per scrivere nel file CSV
+            writer = csv.writer(file)
+            # Apro il file Excel
+            workbook = openpyxl.load_workbook(fileIn)
+
+            # Seleziono il foglio di lavoro che vuoi leggere
+            worksheet = workbook['Abbreviazioni Bibliografiche']
+
+            # Scrivo gli header nel file CSV
+            writer.writerow(headers)
+
+            #Inizializzo un contatore che stamperò nella prima colonna e mi serve da indice
+            contatore = 0
+
+            # Leggo i dati nell'excel ciclicamente e li salvo in un oggetto per poi metterli nel CSV
+            for row in worksheet.iter_rows(min_row=0, min_col=0, max_col=3):
+
+                # Creo una lista vuota per contenere il contenuto delle celle
+                cell_content = []
+
+                # Itero su ogni cella e aggiungi il contenuto alla lista
+                for cell in row:
+                    cell_content.append(cell.value)
+
+                # Stampo il contenuto
+                # print (cell_content)
+
+                # Se c'è qualche valore nella riga allora:
+                if (cell_content[0]):
+                    # Sostituisco il contenuto Null con la stringa 'Missing Value'
+                    for i in range(len(cell_content)):
+                        if cell_content[i] is None:
+                            cell_content[i] = 'Missing Value'
+
+                    # Scrivo il contenuto
+                    writer.writerow([contatore, cell_content[0], cell_content[1], ''])
+                    contatore += 1
     except: 
         print('C\'è stato un errore duranta la creazione del file: \"' + fileOut + '\"')
 
 
 def abbreviazioniTipologiche(fileIn, fileOut):
     try:
-        # DA VEDERE TUTTO
-        print('')
+        # Definisci gli header per il tuo file CSV
+        headers = ['','Abbreviazione','Tipo','']
+
+        # Apri il file CSV in modalità scrittura
+        directory = "dataJohn"  # Specifico la directory
+        filename = fileOut  # Specifico il nome del file
+
+        if not os.path.exists(directory):  # Creo la directory se non esiste
+            os.makedirs(directory)
+
+        filePathAndName = os.path.join(
+            directory, filename)  # Creo il full name path
+
+        with open(filePathAndName, 'w', newline='') as file:
+            # Creo un oggetto writer per scrivere nel file CSV
+            writer = csv.writer(file)
+            # Apro il file Excel
+            workbook = openpyxl.load_workbook(fileIn)
+
+            # Seleziono il foglio di lavoro che vuoi leggere
+            worksheet = workbook['Abbreviazioni Tipologiche']
+
+            # Scrivo gli header nel file CSV
+            writer.writerow(headers)
+
+            #Inizializzo un contatore che stamperò nella prima colonna e mi serve da indice
+            contatore = 0
+
+            # Leggo i dati nell'excel ciclicamente e li salvo in un oggetto per poi metterli nel CSV
+            for row in worksheet.iter_rows(min_row=0, min_col=0, max_col=3):
+
+                # Creo una lista vuota per contenere il contenuto delle celle
+                cell_content = []
+
+                # Itero su ogni cella e aggiungi il contenuto alla lista
+                for cell in row:
+                    cell_content.append(cell.value)
+
+                # Stampo il contenuto
+                # print (cell_content)
+
+                # Se c'è qualche valore nella riga allora:
+                if (cell_content[0]):
+                    # Sostituisco il contenuto Null con la stringa 'Missing Value'
+                    for i in range(len(cell_content)):
+                        if cell_content[i] is None:
+                            cell_content[i] = 'Missing Value'
+
+                    # Scrivo il contenuto
+                    writer.writerow([contatore, cell_content[0], cell_content[1], ''])
+                    contatore += 1
     except: 
         print('C\'è stato un errore duranta la creazione del file: \"' + fileOut + '\"')
 
 
-datiScavo('excel.xlsx', 'dati_anagrafica_clean.csv')
-datiAnagrafica('excel.xlsx', 'dati_scavo_clean.csv')
-datiCollezionistici('excel.xlsx', 'datiCollezionistici_clean.csv')
+
+datiAnagrafica('excel.xlsx', 'dati_anagrafica_clean.csv')
+datiScavo('excel.xlsx', 'dati_scavo_clean.csv')
+datiCollezionistici('excel.xlsx', 'dati_collezionisti_clean.csv')
 datiBibliografici('excel.xlsx', 'dati_bibliografici_clean.csv')
 abbreviazioniArchivi('excel.xlsx', 'abbreviazioni_archivi.csv')
 abbreviazioniBibliografiche('excel.xlsx', 'abbreviazioni_bibliografia.csv')
